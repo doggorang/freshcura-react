f449f75d4f2615d3023e260ba642178850f973d5
Kevin Setiabudi <kevinsetiabudi20@gmail.com>
2023-10-06 15:29:51 +0700

    list of product from AI

--- modules/views/ProductList.tsx
+++ modules/views/ProductList.tsx
@@ -9,9 +9,9 @@ import TextField from '@mui/material/TextField';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Divider from '@mui/material/Divider';
 import Avatar from '@mui/material/Avatar';
-
 import Button from '../components/Button';
 import Typography from '../components/Typography';
+import Grid from '@mui/material/Grid';
 
 const ProductListLayoutRoot = styled('section')(({ theme }) => ({
   color: theme.palette.common.white,
@@ -19,13 +19,44 @@ const ProductListLayoutRoot = styled('section')(({ theme }) => ({
 
 export default function ProductList() {
   //https://devrecipes.net/modal-component-with-next-js/
+  const products = [
+    {
+      id: 1,
+      name: 'LEGO Brand Day!',
+      price: 519000,
+      image: 'https://i.imgur.com/LEGO-Brand-Day.png',
+      category: 'Toys',
+    },
+    {
+      id: 2,
+      name: 'Huawei Band 8',
+      price: 499000,
+      image: 'https://i.imgur.com/Huawei-Band-8.png',
+      category: 'Electronics',
+    },
+    {
+      id: 3,
+      name: 'Kiddy Food Maker 7 in 1 Set',
+      price: 48000,
+      image: 'https://i.imgur.com/Kiddy-Food-Maker.png',
+      category: 'Home & Kitchen',
+    },
+    {
+      id: 4,
+      name: 'Advan Notebook Laptop Workpro',
+      price: 5099000,
+      image: 'https://i.imgur.com/Advan-Notebook-Laptop-Workpro.png',
+      category: 'Electronics',
+    },
+  ];
+  const categories = ['All', 'Toys', 'Electronics', 'Home & Kitchen'];
   return (
     <ProductListLayoutRoot
       sx={{
         backgroundColor: "white",
       }}
     >
-      <Divider>List</Divider>
+      <Divider>.</Divider>
       <Box
         sx={{
           width: 500,
@@ -35,73 +66,31 @@ export default function ProductList() {
         <TextField fullWidth label="fullWidth" id="fullWidth" />
       </Box>
 
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Summer BBQ"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  to Scott, Alex, Jennifer
-                </Typography>
-                {" — Wish I could come, but I'm out of town this…"}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          </ListItemAvatar>
-          <ListItemText
-            primary="Oui Oui"
-            secondary={
-              <React.Fragment>
-                <Typography
-                  sx={{ display: 'inline' }}
-                  component="span"
-                  variant="body2"
-                  color="text.primary"
-                >
-                  Sandra Adams
-                </Typography>
-                {' — Do you have Paris recommendations? Have you ever…'}
-              </React.Fragment>
-            }
-          />
-        </ListItem>
-      </List>
+      <Grid container spacing={2}>
+        {categories.map((category) => (
+          <Grid item key={category}>
+            <Button variant="outlined">{category}</Button>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Grid container spacing={2}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4}>
+            <Box
+              sx={{
+                padding: 2,
+                borderRadius: 1,
+                boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+              }}
+            >
+              <img src={product.image} alt={product.name} />
+              <Typography variant="h6">{product.name}</Typography>
+              <Typography variant="body1">Rp{product.price}</Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
 
     </ProductListLayoutRoot>
   );
